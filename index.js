640b316b7042c3dc91a62c3cf5847a2ed16faf38
Nikhil Gangrade <nikhil12gangrade93@yahoo.com>
2018-06-29 08:25:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function destructivelyUpdateObjectWithKeyAndValue(obect, key, value){
   return obj
 }
 var obj = { prop: 1 }
-destructivelyUpdateObjectWithKeyAndValue(obj, 'prop2', 2)
-obj
+return destructivelyUpdateObjectWithKeyAndValue(obj, 'prop2', 2)
+
