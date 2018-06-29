cba7001a0d410a244d7adbb9c3146ea2851ce5cc
Nikhil Gangrade <nikhil12gangrade93@yahoo.com>
2018-06-29 08:19:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, obj, { [key]: value })
 }
 var obj = {prop : 1}
-updateObjectWithKeyAndValue
+updateObjectWithKeyAndValue(obj, 'prop2', 2)
