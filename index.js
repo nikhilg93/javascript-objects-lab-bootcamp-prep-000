48b7c47a8deeaf88801ad6ce9441a2ff06e03425
Nikhil Gangrade <nikhil12gangrade93@yahoo.com>
2018-06-29 08:47:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,4 +25,7 @@ function deleteFromObjectByKey(object, key){
 function destructivelyDeleteFromObjectByKey(object, key){
    var obj = { prop: 1 }
    var newObj = destructivelyDeleteFromObjectByKey(obj, 'prop');
+   delete newObj,prop
+   newObj
+   obj
 }
